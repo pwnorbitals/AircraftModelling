be936cde81a3c7238b0193263e7f6f61ae2754a4
kmgx <kmgx@declaverie.tech>
2020-11-07 09:48:35 +0100

    add integrator code

--- tp1.py
+++ tp1.py
@@ -1,23 +1,25 @@
 ### AircraftModelling practical works 
 
 endTime = 60 #s
+initialState = []
+integrationStep = 1 
 
-test = True
+def derivatives(state, time):
+    pass
 
 
-
-
-
-def integrator() :
-    return 1
+def integrator_step(state, time, derivatives) :
+        k1 = integrationStep*derivatives(state, currentTime)
+        k2 = integrationStep*derivatives(r + k1/2.0, currentTime + integrationStep/2.0)
+        k3 = integrationStep*derivatives(r + k2/2.0, currentTime + integrationStep/2.0)
+        k4 = integrationStep*derivatives(r + k3, currentTime + integrationStep)
+        r += (k1 + 2.0*k2 + 2.0*k3 + k4)/6.0 
+    return integrationStep
 
 
 currentTime = 0
-state = []
+state = initialState
 if __name__ == "__main__":
     while currentTime < endTime:
-        # Compute state derivatives
-        derivatives = []
-
-        # Integrate state
-        currentTime += integrator(state, derivatives)
+        deltaTime, state = integrator_step(state, currentTime, derivatives)
+        currentTime += deltaTime
