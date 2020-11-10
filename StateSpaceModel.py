2f46b947caf2b674da9c5ec03d84bdb3ada5b739
Baptiste Rubino-Moyner <baptiste.rubino-moyner@ipsa.fr>
2020-11-10 12:31:30 +0100

    Add the last constants, need to be tested

--- StateSpaceModel.py
+++ StateSpaceModel.py
@@ -1,16 +1,17 @@
 import numpy as np
 
-from Tp1_study import r_g, Cz_eq, V_eq, Q, S, Cx_eq, m, new_F_px, new_alpha_eq, Cx_delta_m, l_ref, X, Y
+from Tp1_study import r_g, Cz_eq, V_eq, Q, S, Cx_eq, m, new_F_px, new_alpha_eq, Cx_delta_m, l_ref, X, Y, k, Cz_alpha
 
-Cx_alpha = 1 # TBD
+
 Cz_alpha = 2.2
-gamma_eq = 1 # TBD
-Ftho = 1 # TBD
+gamma_eq = 0
+Ftho = 0
 Cm_q = -0.27
 IYY = m * r_g**2
 
 Cz_delta_m = 0.35
 
+Cx_alpha = 2*k*Cz_eq*Cz_alpha
 Cm_alpha = X / l_ref * (Cx_alpha * np.sin(new_alpha_eq) + Cz_alpha * np.cos(new_alpha_eq))
 Cm_delta_m = Y / l_ref * (Cx_delta_m * np.sin(new_alpha_eq) + Cz_delta_m * np.cos(new_alpha_eq))
 
@@ -18,6 +19,7 @@ Cm_delta_m = Y / l_ref * (Cx_delta_m * np.sin(new_alpha_eq) + Cz_delta_m * np.co
 g0 = -9.81
 
 Xv = 2 * Q *S * Cx_eq / (m * V_eq) #
+
 Xalpha = new_F_px / (m * V_eq) * np.sin(new_alpha_eq) + Q*S*Cx_alpha/(m*V_eq) #
 Xgamma = g0 * np.cos(gamma_eq) / V_eq
 Xdelta_m = Q *S* Cx_delta_m / (m * V_eq)
