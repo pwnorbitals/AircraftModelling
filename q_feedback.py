a1145345c586edd001dd45a8f82306282c4b782e
kmgx <kmgx@declaverie.tech>
2020-12-18 11:29:57 +0100

    merge

--- q_feedback.py
+++ q_feedback.py
@@ -1,10 +1,11 @@
 import numpy as np
 import control
 from StateSpaceModel import A_reduced, B_reduced, Cq, Dq
+from sisopy31 import *
 
 Kr = -0.18817
 
-Ak = A_reduced - Kr * np.dot(B_reduced,Cq)
+Ak = A_reduced - (Kr * np.dot(B_reduced,Cq))
 Bk = Kr * B_reduced
 Ck = Cq # As we want q as an output
 Dk = Dq # = 0
@@ -15,7 +16,12 @@ res = control.damp(sys)
 tf_k= control.tf(sys)
 
 
-print("Transfert function : ", tf_k)
+print("Transfer function : ", tf_k)
 print("pulsation 1 : ", res[0][0]*2*np.pi, " rad/s")
 print("pulsation 2 : ", res[0][1]*2*np.pi, " rad/s")
 print("pulsation 3 : ", res[0][2]*2*np.pi, " rad/s")
+
+sys = control.ss(Ak, Bk, Ck, Dk)
+
+if __name__ == "__main__":
+    sisotool(sys)
